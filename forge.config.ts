2a84e4210010e23d326a15da2aaf43013441c569
Vitaly Turovsky <vital2580@icloud.com>
2025-08-02 04:59:51 +0300

    add icon!

--- forge.config.ts
+++ forge.config.ts
@@ -15,6 +15,7 @@ import { rendererConfig } from './webpack.renderer.config';
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
+    icon: './icon.png',
   },
   rebuildConfig: {},
   makers: [
